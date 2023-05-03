cec0c27c42fdff6e8a5952f9e8588efa44e3a79a
Sergio Solarte <sergiosolarte529@hotmail.com>
2023-05-02 20:00:39 -0500

    Complete excercise 2, despite off JTest file was not working good

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
 
+  
+const repeatString = function(string, num) {
+    let ans="";
+    if (num<0) {
+        return 'ERROR';
+    }
+    for (let i = 0; i < num; i++) {
+        ans += string;
+    }
+    return ans;
 };
 
 // Do not edit below this line
--- 02_repeatString/solution/repeatString-solution.js
+++ 02_repeatString/solution/repeatString-solution.js
@@ -1,3 +1,5 @@
+
+
 const repeatString = function (word, times) {
   if (times < 0) return "ERROR";
   let string = "";
