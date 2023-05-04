b20093b0359235ac5e3528a62714a21ea6173f4b
Sergio Solarte <sergiosolarte529@hotmail.com>
2023-05-03 20:22:07 -0500

    'Complete excercise 3 however not able to pass the first test buged (?)/

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,15 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let ans=[];
+    let arrString=string.split('');
+    for (let i = arrString.length-1; i >= 0; i--) {
+        ans.push(arrString[i]);
+        
+    }
+    ans=ans.join('');
+    return ans;
+    
 };
+reverseString('hello')
 
 // Do not edit below this line
 module.exports = reverseString;
--- 03_reverseString/solution/reverseString-solution.js
+++ 03_reverseString/solution/reverseString-solution.js
@@ -1,5 +1,16 @@
-const reverseString = function (string) {
+
+const reverseString = function(string) {
+  let ans=[];
+  let arrString=string.split('');
+  for (let i = arrString.length-1; i >= 0; i--) {
+      ans.push(arrString[i]);
+      
+  }
+  ans=ans.join('');
+  return ans;
+  
+};/*const reverseString = function (string) {
   return string.split("").reverse().join("");
-};
+};*/
 
 module.exports = reverseString;
