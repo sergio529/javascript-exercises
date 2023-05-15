6955b5ff2d573253392838481b13a8e61f7fd92f
Sergio Solarte <sergiosolarte529@hotmail.com>
2023-05-15 18:54:54 -0500

    Complete excercise 05

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -2,14 +2,18 @@
   
 const repeatString = function(string, num) {
     let ans="";
+    
     if (num<0) {
         return 'ERROR';
     }
     for (let i = 0; i < num; i++) {
         ans += string;
     }
+   
     return ans;
+    
 };
 
+repeatString('hey',3)
 // Do not edit below this line
 module.exports = repeatString;
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(a,b) {
+    let sum=0;
+    if (a>b) {
+        let c=a;
+        a=b;
+        b=c;
+    }
+    if( a < 0 || b < 0 ||typeof a != 'number' || typeof b != 'number'){
+        return 'ERROR';
+    }
+    for (let i = 0; i <= b-a; i++) {
+        sum += a+i;
+        
+    }
+    console.log(a);
+    return sum;
 };
-
+console.log(sumAll());
 // Do not edit below this line
 module.exports = sumAll;
